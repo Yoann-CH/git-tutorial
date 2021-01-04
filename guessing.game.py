1283d873c78b147ed76be01ff44fc30c5f3b1b78
Yoann CHAMBEUX <yoann.chambeux@edu.devinci.com>
2021-01-04 14:20:54 +0100

    jeu recherche de nombre

--- guessing.game.py
+++ guessing.game.py
@@ -8,7 +8,9 @@ def guess_loop():
 	#This is the number the user will have to guess, chosen randomly in betw	een 1 and 100
 	number_to_guess = random.randint (1, 100)
 	print("I have in mind a number in between 1 and 100, can you find it?")
-	
+	print("Choose an username")
+	name = input()
+	print("Now, choose a number")
 	#Replay the question until the user finds the correct number
 	while True:
 		try:
@@ -22,7 +24,7 @@ def guess_loop():
 				print("The number to guess is higher")
 			else:
 				#The user found the number to guess, let's exit
-				print("You just found the number, it was indeed"				)
+				print( "Good job",name,",it is the good number"				)
 				return
 		#A ValueError is raised by the int() function if the user inputs		something else than a number
 		except ValueError as err:
